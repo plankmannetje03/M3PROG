0d21c1b2c0a34c91ab8d40cb6a6d251523fd4d48
plankmannetje03 <38730@ma-web.nl>
2025-02-14 10:33:53 +0100

    echo's verwijderen

--- M3PROG_projecten/public/01/phphtml_var.php
+++ M3PROG_projecten/public/01/phphtml_var.php
@@ -5,11 +5,7 @@ $tijd = date("H:i");
 $favorieteGame = "Super metroid";
 $mooisteGetal = 42;
 
-echo $title;
-echo $datum;
-echo $tijd;
-echo $favorieteGame;
-echo $mooisteGetal;
+
 
 ?>
 
@@ -17,11 +13,11 @@ echo $mooisteGetal;
 <!doctype html>
 <html lang="en">
     <head>
-        <title><?php echo "Titel van de pagina" ?></title>
+        <title><?php echo $title ?></title>
     </head>
     <body>
-        <h2>De datum vandaag is: <?php echo date('d-m-Y')?>, en de tijd is <?php echo date("H:i")?> uur.
+        <h2>De datum vandaag is: <?php echo $datum?>, en de tijd is <?php echo $tijd?> uur.
         
-        <p><h2>Mijn favoriete game</h2><?php echo "Super metroid";?></p>
+        <p><h2>Mijn favoriete game</h2><?php echo $mooisteGetal ;echo $favorieteGame;?></p>
     </body>
 </html>
