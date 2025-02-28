1218473b778749922f0ec7b335109eaca9a59fa9
plankmannetje03 <38730@ma-web.nl>
2025-02-28 10:28:17 +0100

    simplecalc

---
+++ M3PROG_projecten/public/02/simplecalc.php
@@ -0,0 +1,17 @@
+<?php
+$som1 = 2 + 2;
+$som2 = 5 * 5;
+$som3 = 100/10;
+$som4 = 60 - 25; 
+$varsom1 = $som1 * $som2;
+$varsom2 = $som4/$som3;
+$varsom3 = $varsom1 * $som4/$varsom2 - $som2;
+
+echo  "$som1 <br/>"; 
+echo  "$som2 <br/>"; 
+echo  "$som3 <br/>"; 
+echo  "$som4 <br/>"; 
+echo "$varsom1 <br/>";
+echo "$varsom2 <br/>";
+echo "$varsom3 <br/>";
+?>
